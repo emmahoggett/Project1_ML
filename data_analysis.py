ef83eff6a77fc55bb2c1ca9e97cd4e458ba0def9
louiseaubet <55659486+louiseaubet@users.noreply.github.com>
2019-10-17 14:49:08 +0200

    ajout de build_poly

--- data_analysis.py
+++ data_analysis.py
@@ -62,3 +62,13 @@ def split_data(x, y, ids, ratio, seed=1):
     ids_test = ids[index_test]
     return x_train, x_test, y_train, y_test, ids_train, ids_test
 
+
+# Extend the features 
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    poly = np.ones((len(x), 1))
+    for deg in range(1, degree+1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
+
