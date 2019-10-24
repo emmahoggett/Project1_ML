95456ecd6a7dce54da648ba24d0832196bf181a2
emmahoggett <emma.hoggett@epfl.ch>
2019-10-24 16:18:20 +0200

    resultat reg/log

--- cross_validation_logistic.py
+++ cross_validation_logistic.py
@@ -1,30 +1,39 @@
-def cross_validation_lambda(y, tX, w0, max_iters, gamma, k_fold=4,):
+import numpy as np
+from proj1_helpers import *
+from data_analysis_logistic import *
+from implementation import *
+
+
+def cross_validation_deg_logistic(y, tX, max_iters, gamma, k_fold=4, max_degree=5):
     seed = 1
     #lambdas = [0, 0.1, 0.15, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5]
-    lambdas = np.logspace(-4,1,20)
+    degrees=range(1,max_degree)
     # split data in k fold
     k_indices = build_k_indices(y, k_fold, seed)
     # define lists to store the loss of training data and test data
-    rmse_tr = []
-    rmse_te = []
+    
+    
+    optimal_deg=np.ones(tX.shape[1],np.int64)
     # cross validation
-    for ind, lambda_ in enumerate(lambdas):
-        loss_tr = 0
-        loss_te = 0
-        for k in range(k_fold):
-            l_tr, l_te = cross_validation(y, tX, k_indices, k, lambda_, w0, max_iters, gamma)
-            loss_tr = loss_tr + l_tr
-            loss_te = loss_te + l_te
-        loss_tr = loss_tr/(k_fold)
-        loss_te = loss_te/(k_fold)
-        rmse_tr.append(loss_tr)
-        rmse_te.append(loss_te)
-    optimal_lambda = lambdas[np.argmin(rmse_te)]    
-    cross_validation_visualization(lambdas, rmse_tr, rmse_te)
-    return optimal_lambda
+    for ind in range(tX.shape[1]):
+        rmse_te = []
+        for degree_ in degrees:
+            grade_te = 0
+            optimal_deg[ind]=degree_
 
-def cross_validation(y, x, k_indices, k, lambda_, w0, max_iters, gamma):
+            for k in range(k_fold):
+                gr_te = cross_validation_logistic_poly(y, tX, k_indices, k, max_iters, gamma,optimal_deg)
+                grade_te = grade_te + gr_te
+
+            grade_te = grade_te/(k_fold)
+            rmse_te.append(grade_te)
+
+        optimal_deg[ind] = degrees[np.argmax(rmse_te)]
+    return optimal_deg
+
+def cross_validation_logistic_poly(y, x, k_indices, k, max_iters, gamma,deg):
     # get k'th subgroup in test, others in train
+    
     te_indices = k_indices[k]
     tr_indices = k_indices[~(np.arange(k_indices.shape[0]) == k)]
     tr_indices = tr_indices.reshape(-1)
@@ -34,12 +43,25 @@ def cross_validation(y, x, k_indices, k, lambda_, w0, max_iters, gamma):
     y_te = y[te_indices]
     tx_tr = x_tr
     tx_te = x_te
-    #tx_tr = build_poly(x_tr, degree)
-    #tx_te = build_poly(x_te, degree)
-    # ridge regression
-    weight, _ = reg_logistic_regression(y_tr, tx_tr, lambda_, w0, max_iters, gamma)
     
+    tx_te_tmp=build_multi_poly(tx_te,deg)
+    tx_tr_tmp=build_multi_poly(tx_tr,deg)
+    
+    w0=np.zeros(tx_tr_tmp.shape[1])
+    weight, _= logistic_regression(y_tr, tx_tr_tmp,w0, max_iters, gamma)
     # calculate the loss for train and test data
-    rmse_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weight))
-    rmse_te = np.sqrt(2 * compute_loss(y_te, tx_te, weight))
-    return rmse_tr, rmse_te
+    y_pred = predict_labels(weight, tx_te_tmp)
+    res = np.where(y_te[:,] == y_pred[:,], 1, 0)
+    grade = np.mean(res)
+    
+    return grade
+
+def build_k_indices(y, k_fold, seed):
+    # build k indices for k-fold 
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
--- data_analysis_logistic.py
+++ data_analysis_logistic.py
@@ -1,26 +1,41 @@
+from implementation import*
+from proj1_helpers import *
+from scipy import stats
+
 import numpy as np
 
 ##### Exploratory data analysis #####
 
 def data_analysis(jet_num, y_tr, tX_tr, ids_tr, y_fin, tX_fin, ids_fin,ratio=0.8):
     
+    y_tr=rescale_y(y_tr, ids_tr, -1, 0)
+    y_fin=rescale_y(y_fin, ids_fin, -1,0)
+    
     y_tr, tX_tr, ids_tr, _ = extract_jet_num(jet_num, y_tr, tX_tr, ids_tr)
     y_fin, tX_fin, ids_fin, _ = extract_jet_num(jet_num, y_fin, tX_fin, ids_fin)
     
-    tX_tr = extract_values(tX_tr, ids_tr)
-    tX_fin = extract_values(tX_fin, ids_fin)
+    tX_tr = extract_999(tX_tr)
+    tX_tr_new, tX_te, y_tr_new, y_te, ids_tr_new, ids_te=split_data(tX_tr, y_tr, ids_tr, ratio, seed=1)
     
-    tX_tr, tX_te, y_tr, y_te, ids_tr, ids_te=split_data(tX_tr, y_tr, ids_tr, ratio, seed=1)
     
-    tX_tr, m, std = standardize(tX_tr)
+    tX_tr_new, m, std = standardize(tX_tr_new)
+    tX_tr=standardize_te(tX_tr, m, std)
     tX_te = standardize_te(tX_te, m, std)
     tX_fin = standardize_te(tX_fin, m, std)
+    
+    tX_tr_new, ids_tr_new,y_tr_new=extract_outliers(tX_tr_new,ids_tr_new,y_tr_new)
+    tX_tr, ids_tr,y_tr=extract_outliers(tX_tr, ids_tr,y_tr)
+    tX_te, ids_te, y_te=extract_outliers(tX_te, ids_te, y_te)
+    
+    y_tr=rescale_y(y_tr, ids_tr, -1, 0)
+    y_fin=rescale_y(y_fin, ids_fin, -1,0)
+    
     
     #tX_tr=build_multi_poly(tX_tr, degree)
     #tX_te=build_multi_poly(tX_te, degree)
     #tX_fin=build_multi_poly(tX_fin, degree)
     
-    return y_tr, tX_tr, ids_tr, y_te, tX_te, ids_te, y_fin, tX_fin, ids_fin
+    return y_tr, tX_tr,y_tr_new, tX_tr_new, ids_tr, y_te, tX_te, ids_te, y_fin, tX_fin, ids_fin
 
 # Extract the data points with the same number of jets
 
@@ -30,30 +45,42 @@ def extract_jet_num(jet_num, y, tX, ids):
     tX_jet_num = tX[is_jet_num,:]
     if (jet_num == 0):
         #is_not_999 = np.delete(np.arange(30), [4, 5, 6, 12, 22, 23, 24, 26, 27, 29])
-        is_not_999 = np.delete(np.arange(30), [4, 5, 6, 12, 22, 23, 24, 25, 26, 27, 28, 29]) 
+        is_not_999 = np.delete(np.arange(30), [ 4, 5, 6, 12, 22, 23, 24, 25, 26, 27, 28, 29]) 
     elif (jet_num == 1):
         #is_not_999 = np.delete(np.arange(30), [4, 5, 6, 12, 22, 26, 27])
-        is_not_999 = np.delete(np.arange(30), [4, 5, 6, 8, 12, 22, 25, 26, 27, 28])
+        is_not_999 = np.delete(np.arange(30), [ 4, 5, 6, 8, 12, 22, 25, 26, 27, 28])
     else: 
         #is_not_999 = np.delete(np.arange(30), [22])
-        is_not_999 = np.delete(np.arange(30), [8, 22, 25, 28])
+        is_not_999 = np.delete(np.arange(30), [ 8, 22, 25, 28])
     new_tX = tX_jet_num[:,is_not_999] #We take out the values at -999
     new_ids = ids[is_jet_num]
     return new_y, new_tX, new_ids, tX_jet_num
 
 # Remaining -999 values
-def extract_values(tX, ids):
-    
+def extract_999(tX):
     for i in range(tX.shape[1]):
-        is_undefined = tX[:,i] == -999
-        mean = tX[is_undefined == False, i].mean()
-        new_tX = np.copy(tX)
+        is_defined = np.where(tX[:,i] != -999, True, False)
+        tX_defined = tX[is_defined,i]
+        mean = tX_defined.mean()
+        for j in range(tX.shape[0]):
+            if (tX[j,i] == -999):
+                tX[j,i] = mean
+    return tX
+
+def rescale_y(y, ids, val, new_val):
+    " y in the span 0 to 1"
+    for i in range(y.shape[0]):
+        if (y[i]==val):
+            y[i]==new_val
+    return y
+
+def extract_outliers(tX,ids,y):
+    z =np.abs(tX)
+    y=y[(z < 3).all(axis=1)]
+    ids=ids[(z < 3).all(axis=1)]
+    tX=tX[(z < 3).all(axis=1)]
         
-        for j in range(new_tX.shape[0]):
-            if is_undefined[j]:
-                new_tX[j,i] = mean
-        
-    return new_tX
+    return tX,ids,y
 
 #Standardize the original dataset 
 
@@ -98,11 +125,16 @@ def build_poly(x, degree):
     return poly
 
 def build_multi_poly(x, degrees):
-    for i in reversed(range(x.shape[0])):
+    for i in reversed(range(len(degrees))):
         x = np.c_[x[:,:i], build_poly(x[:,i],degrees[i]), x[:,i+1:]]
         #poly = build_poly(X[:,i],degrees[i])
         #multi_poly = np.c_[multi_poly, poly]
     tx = np.c_[np.ones((x.shape[0], 1)), x]
     return tx
 
-
+def build_poly_data(tX_tr,tX_tr_new,tX_te,tX_fin,degree):
+    tX_tr=build_multi_poly(tX_tr, degree)
+    tX_tr_new=build_multi_poly(tX_tr_new, degree)
+    tX_te=build_multi_poly(tX_te, degree)
+    tX_fin=build_multi_poly(tX_fin, degree)
+    return tX_tr,tX_tr_new,tX_te,tX_fin
